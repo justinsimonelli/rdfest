24d0ae5b7e9151f9f9a55f07817b4a8689e0b94e
Justin Simonelli <justin.simonelli@gmail.com>
2015-03-29 21:00:54 -0400

    Adding new route, adding new paypal button

--- routes.py
+++ routes.py
@@ -1,5 +1,5 @@
 # routes.py
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route("/")
@@ -13,6 +13,13 @@ def payment():
 @app.route("/thanks/")
 def thanks():
     return render_template("thanks.html")
+
+@app.route("/update-user/", methods=['GET'])
+def update():
+    person = request.args.get('person');
+    status = request.args.get('status');
+    newPerson = request.args.get('addPerson')
+    return "good"
 
 @app.route("/test/")
 def test():
